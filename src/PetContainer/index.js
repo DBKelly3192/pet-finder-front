0f9cdc3ea2fb48bffd7e77022f2ceea566e9e436
David Kelly <DavidBenjaminKelly@gmail.com>
2020-11-19 16:56:20 -0500

    fixed a bunch of little errors, couldn't tell you where or what

--- src/PetContainer/index.js
+++ src/PetContainer/index.js
@@ -13,7 +13,9 @@ export default class PetContainer extends Component {
 
   createUser = async (userToAdd) => {
     try {
-      const url = process.env.API_URL + '/api/v1/users/register/'
+      // const url = process.env.REACT_APP_API_URL + '/api/v1/users/register/'
+      const url = 'http://localhost:8000/api/v1/users/register'
+      console.log(url)
       const createUserResponse = await fetch(url, {
         method: 'POST',
         headers: {
@@ -22,8 +24,10 @@ export default class PetContainer extends Component {
         body: JSON.stringify(userToAdd)
       })
       const createUserJson = await createUserResponse.json()
+      console.log(createUserJson)
       if (createUserResponse.status === 200 || createUserResponse.status === 201) {
         console.log('USER CREATED')
+        console.log('CREATED USER', createUserJson.data)
       }
     } catch(err) {
       console.log('ERROR CREATING USER', err)
